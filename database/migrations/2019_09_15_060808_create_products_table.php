8a46a4a1119ad715c098e6825314258f55f441e1
Frank <frank.mailservices@gmail.com>
2019-09-15 12:08:45 +0530

    created orders and products table migration files

---
+++ database/migrations/2019_09_15_060808_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name',50)->nullable();
+            $table->string('sku',50)->unique();
+            $table->integer('quantity')->nullable();
+            $table->string('description',5000)->nullable();
+            $table->json('options')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
